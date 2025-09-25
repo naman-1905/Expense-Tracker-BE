b04e01b39e4a687d544b54d30cb71790610031fc
naman-1905 <namanoj19@gmail.com>
2025-09-25 11:56:55 +0000

    Updated SQL queries to create tables for users and transactions

--- db/init.sql
+++ db/init.sql
@@ -1,22 +1,17 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE income (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    amount NUMERIC(12, 2) NOT NULL,
-    category VARCHAR(50),
-    date DATE DEFAULT CURRENT_DATE
-);
-
-CREATE TABLE expense (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    amount NUMERIC(12, 2) NOT NULL,
-    category VARCHAR(50),
-    date DATE DEFAULT CURRENT_DATE
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    amount INT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type VARCHAR(20) CHECK (type IN ('income', 'expense')) NOT NULL,
+    emoji VARCHAR(10),
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE
 );
