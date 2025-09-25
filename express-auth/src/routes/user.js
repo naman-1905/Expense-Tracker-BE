e10d7eb5627e72851ec58407128e86c1c1a72528
naman-1905 <namanoj19@gmail.com>
2025-09-25 15:04:32 +0000

    Restructured

--- express-auth/src/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getUserInfo } = require("../controllers/userController");
-const router = express.Router();
-
-router.get("/:id", getUserInfo);
-
-module.exports = router;
--- flask-history/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from routes.history import history_bp
-
-app = Flask(__name__)
-app.register_blueprint(history_bp, url_prefix="/history")
-
-if __name__ == "__main__":
-    app.run(port=5002, debug=True)
