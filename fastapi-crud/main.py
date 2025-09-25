9330cfa0f3e714de15d96268ebbb85ad6894d517
naman-1905 <namanoj19@gmail.com>
2025-09-25 14:00:04 +0000

    Updated main.py with FastAPI port

--- fastapi-crud/main.py
+++ fastapi-crud/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from routers import transaction
+import os
+import uvicorn
 
 app = FastAPI(title="Transaction CRUD API")
-
 app.include_router(transaction.router, prefix="/transactions", tags=["Transactions"])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))  # default 5001
+    uvicorn.run(app, host="0.0.0.0", port=port)
