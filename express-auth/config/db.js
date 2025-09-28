ff0831485f12e5ff00977713c259079a2c89f41c
naman-1905 <namanoj19@gmail.com>
2025-09-28 04:04:12 +0000

    Updated db to search for the expense schema

--- express-auth/config/db.js
+++ express-auth/config/db.js
@@ -8,14 +8,20 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
 });
 
-// Test database connection
+// Test database connection and set schema
 pool.connect((err, client, release) => {
   if (err) {
     console.error('Error acquiring client:', err.stack);
     return;
   }
-  console.log('Connected to PostgreSQL database');
-  release();
+  client.query('SET search_path TO expense', (err2) => {
+    if (err2) {
+      console.error('Error setting search_path:', err2.stack);
+    } else {
+      console.log('Connected to PostgreSQL database (schema: expense)');
+    }
+    release();
+  });
 });
 
 module.exports = pool;
