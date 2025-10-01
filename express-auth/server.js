3927c6df09eb431ec96cc5a30984eb43ae0ae8cd
naman-1905 <namanoj19@gmail.com>
2025-10-01 16:13:27 +0000

    Update CORS middleware to allow requests from the production domain

--- express-auth/server.js
+++ express-auth/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://expenses.halfskirmish.com'],
   credentials: true
 }));
 
